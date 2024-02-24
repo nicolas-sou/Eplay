62bf0cddf5581428e67a67050c5b7fb31f812698
Nicolas Sousa <nicollassouza141@gmail.com>
2024-02-23 22:02:09 -0300

    Ajuste 1

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Action, Item, Items, Modal, ModalContent } from './styles'
 
-import Section from '../Section'
+import Section from '../../components/Section'
 
 import play from '../../assets/images/play.png'
 import zoom from '../../assets/images/zoom.png'
